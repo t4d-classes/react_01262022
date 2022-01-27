6ddf091766e5aa5dee7b2bd5e995d9d069f99a98
Eric Greene <eric@t4d.io>
2022-01-27 15:31:19 -0500

    add solution to exercise 8

--- demo-app/src/components/CarViewRow.js
+++ demo-app/src/components/CarViewRow.js
@@ -1,13 +1,23 @@
 export const CarViewRow = (props) => {
 
+  // const deleteCar = () => {
+  //   props.onDeleteCar(props.car.id);
+  // }
+
   return (
     <tr>
+      {/* React.createElement('td', { children: props.car.id }) */}
       <td>{props.car.id}</td>
       <td>{props.car.make}</td>
       <td>{props.car.model}</td>
       <td>{props.car.year}</td>
       <td>{props.car.color}</td>
       <td>{props.car.price}</td>
+      <td>
+        {/* <button type="button" onClick={deleteCar}>Delete</button> */}
+        <button type="button"
+          onClick={() => props.onDeleteCar(props.car.id)}>Delete</button>
+      </td>
     </tr>
   )
 }
